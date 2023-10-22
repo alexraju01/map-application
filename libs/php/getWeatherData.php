ed1f1290f18f4c1059f029dc9a1004742bf0244c
CodeExcalibar <Alex.raju1010@gmail.com>
2023-10-22 08:17:15 +0100

    debugging

--- libs/php/getWeatherData.php
+++ libs/php/getWeatherData.php
@@ -9,7 +9,7 @@ $executionStartTime = microtime(true);
 $api = 'https://api.openweathermap.org/data/2.5/weather?q='.$_REQUEST['cityNames'].','. $_REQUEST['countryCodes']. '&appid=f60bdf556374ad06eba301b79765e1fa';
 // Initialize a CURL session and set options to make the HTTP request
 $ch = curl_init();
-// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  // Ignore the SSL certification return a string
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  // Ignore the SSL certification return a string
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_URL, $api);
 
